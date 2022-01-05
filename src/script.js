5f07d3a8926a528246923f7647dd1b90cf5c863b
Edward Hinrichsen <edward.w.hinrichsen@gmail.com>
2022-01-05 19:03:10 +1100

    add outh

--- src/script.js
+++ src/script.js
@@ -28,7 +28,7 @@ const plane = new THREE.Mesh( planelikeGeometry, new THREE.MeshBasicMaterial( { 
 // const plane = new THREE.Mesh( planelikeGeometry, new THREE.MeshBasicMaterial( { color: 'red' } ) );
 
 // plane.position.set(0,100,-500);
-scene.add(plane);
+// scene.add(plane);
 
 /**
  * Textures
@@ -40,118 +40,19 @@ const flagTexture = textureLoader.load("/textures/flag-french.jpg");
  * Test mesh
  */
 // Geometry
-const geometry = new THREE.PlaneGeometry(2, 2, 800, 800);
-const count = geometry.attributes.position.count;
-const randoms = new Float32Array(count);
-for (let i = 0; i < count; i++) {
-  randoms[i] = Math.random();
-}
-geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
+const backGround = new THREE.Mesh(new THREE.PlaneGeometry(2, 2, 1, 1), new THREE.MeshBasicMaterial({ color: 'red' }))
+backGround.position.set(0,0,-0.01)
+sceneRTT.add(backGround)
+
+sceneRTT.add( new THREE.AxesHelper( 1 ));
+// scene.add(backGround)
 
 // Material
-// const material = new THREE.MeshBasicMaterial()
-// canvas.getContext();
-// WebGLRenderingContext.getSupportedExtensions()
-// canvas.getExtension('OES_standard_derivatives');
-const gl = canvas.getContext("webgl");
-gl.getExtension("OES_standard_derivatives");
-// console.log(gl)
-const material = new THREE.RawShaderMaterial({
-  vertexShader: testVertexShader,
-  fragmentShader: testFragmentShader,
-  uniforms: {
-    uTime: { value: 0 },
-
-    // uWindDirMin: {value: 3.14/4.0},
-    // uWindDirMax: {value: 3.14/3.0},
-
-    // uWaveLenMin: {value: 0.1},
-    // uWaveLenMax: {value: 0.15},
-
-    // uAmplitudeMin: {value: 0.05},
-    // uAmplitudeMax: {value: 0.1},
-
-    // uSlopeMin: {value: 0.05},
-    // uSlopeMax: {value: 0.7},
-
-    uWindDirMin: { value: new THREE.Vector4(6.28 * (-0.65) ,6.28 * (0.95),6.28 * 0.7, 6.28 * (0.1)) },
-    uWindDirMax: { value: new THREE.Vector4(6.28 * (-0.7)  ,6.28 * 1    ,6.28 * 0.7 , 6.28 * (0.125)) },
-
-    uWaveLenMin: { value: new THREE.Vector4(0.7, 0.25, 1,   0.66) },
-    uWaveLenMax: { value: new THREE.Vector4(1,    0.4, 1.5, 0.33) },
-
-    uAmplitudeMin: { value: new THREE.Vector4(0.5, 0.15,0.5, 0.5) },
-    uAmplitudeMax: { value: new THREE.Vector4(0.7, 0.3,1, 0.01) },
-
-    uSlopeMin: { value: new THREE.Vector4(0.8, 0.8,0.4, 1) },
-    uSlopeMax: { value: new THREE.Vector4(1, 1     ,0.64, 0.3) },
-
-    uSpeed: { value: new THREE.Vector4(0.5*0.6, 1 *0.6    ,1.5*0.6, 2*0.6) },
-    uFlutterSpeed: { value: new THREE.Vector4(0.5*0.6, 1*0.6     ,3*0.6, 0.2*0.6) },
-  },
-});
-
-gui.close()
-for (let i = 0; i < 4; i++) {
-  const waveGUI = gui.addFolder("Wave " + i);
-
-  let prop;
-
-  switch (i) {
-    case 0:
-      prop = "x";
-      break;
-
-    case 1:
-      prop = "y";
-      break;
-    case 2:
-      prop = "z";
-      break;
-    case 3:
-      prop = "w";
-      break;
-  }
-
-  const WindDir = waveGUI.addFolder("WindDir");
-  WindDir.add(material.uniforms.uWindDirMin.value, prop)
-    .min(-2 * Math.PI)
-    .max(2 * Math.PI)
-    .name("Min");
-  WindDir.add(material.uniforms.uWindDirMax.value, prop)
-    .min(-2 * Math.PI)
-    .max(2 * Math.PI)
-    .name("Max");
-
-  const WaveLen = waveGUI.addFolder("WaveLen");
-  WaveLen.add(material.uniforms.uWaveLenMin.value, prop)
-    .min(0.001)
-    .max(2)
-    .name("Min");
-  WaveLen.add(material.uniforms.uWaveLenMax.value, prop)
-    .min(0.001)
-    .max(2)
-    .name("Max");
-
-  const Amplitude = waveGUI.addFolder("Amplitude");
-  Amplitude.add(material.uniforms.uAmplitudeMin.value, prop)
-    .min(0)
-    .max(1)
-    .name("Min");
-  Amplitude.add(material.uniforms.uAmplitudeMax.value, prop)
-    .min(0)
-    .max(1)
-    .name("Max");
-
-  const Slope = waveGUI.addFolder("Slope");
-  Slope.add(material.uniforms.uSlopeMin.value, prop).min(0).max(1).name("Min");
-  Slope.add(material.uniforms.uSlopeMax.value, prop).min(0).max(1).name("Max");
-}
-console.log(new THREE.Vector4(0.7, 0.7));
 
 // Mesh
-const mesh = new THREE.Mesh(geometry, material);
-sceneRTT.add(mesh);
+// const mesh = new THREE.Mesh(geometry, material);
+// sceneRTT.add(mesh);
+
 
 /**
  * Sizes
@@ -188,16 +89,21 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(0.25, -0.25, 1);
 scene.add(camera);
 
-const cameraRTT = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-cameraRTT.position.set(0.25, -0.25, 1);
-sceneRTT.add(cameraRTT);
+const cameraRTT = new  THREE.OrthographicCamera( sizes.width * 0.01 / (- 2), sizes.width * 0.01 / 2, sizes.height * 0.01 / 2, sizes.height* 0.01 / (- 2), 1, 1000 );
+// cameraRTT.position.set(0, 0, 0);
+// sceneRTT.add(cameraRTT);
+
+// const cameraRTT = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.1,
+//   100
+// );
+// cameraRTT.position.set(0.25, -0.25, 1);
+cameraRTT.position.set(0, 0, 1);
 
 // Controls
+// const controls = new OrbitControls(cameraRTT, canvas);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
@@ -222,10 +128,6 @@ const tick = () => {
   controls.update();
   // const elapsedTime = clock.getElapsedTime()
 
-  // Update material
-  material.uniforms.uTime.value = elapsedTime;
-
-  // Render
   
   // Render first scene into texture
 
@@ -243,7 +145,8 @@ const tick = () => {
   // (using first scene as regular texture)
 
   renderer.setRenderTarget( null );
-  renderer.render( scene, camera );
+  // renderer.render( scene, camera );
+  renderer.render( sceneRTT, cameraRTT );
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
