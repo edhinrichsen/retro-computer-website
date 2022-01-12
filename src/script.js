8d44b91022d87f9af7ad54d4915cc6ddeadce513
Edward Hinrichsen <edward.w.hinrichsen@gmail.com>
2022-01-12 11:47:37 +1100

    add blome

--- src/script.js
+++ src/script.js
@@ -5,6 +5,10 @@ import * as dat from "lil-gui";
 import testVertexShader from "./shaders/test/vertex.vert";
 import testFragmentShader from "./shaders/test/fragment.frag";
 import { Uniform } from "three";
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+// import { BloomPass } from 'three/examples/jsm/postprocessing/BloomPass.js';
 
 /**
  * Base
@@ -15,19 +19,93 @@ const gui = new dat.GUI();
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
+
 // Scene
 const scene = new THREE.Scene();
 const sceneRTT = new THREE.Scene();
+/**
+ * Sizes
+ */
+ const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-const rtTexture = new THREE.WebGLRenderTarget(512, 512, {
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(0.25, -0.25, 1);
+scene.add(camera);
+
+const cameraRTT = new THREE.OrthographicCamera(-0.1, 1.1, 0.1, -1.1, 1, 3);
+sceneRTT.add(cameraRTT);
+cameraRTT.position.set(0, 0, 1);
+
+// Controls
+// const controls = new OrbitControls(cameraRTT, canvas);
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+
+ const rtTexture = new THREE.WebGLRenderTarget(512, 512, {
   format: THREE.RGBFormat,
 });
+
+ const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// renderer.render(sceneRTT, cameraRTT);
+
+const composer = new EffectComposer( renderer, rtTexture );
+composer.renderToScreen = false
+
+// render(sceneRTT, cameraRTT);
+const renderPass = new RenderPass( sceneRTT, cameraRTT );
+composer.addPass( renderPass );
+
+// const bloomPass = new BloomPass()
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( 512, 512 ), 1, 0.4, 0 );
+composer.addPass( bloomPass );
+
+
+
+
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+
+
 
 const planelikeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const plane = new THREE.Mesh(
   planelikeGeometry,
-  new THREE.MeshBasicMaterial({ map: rtTexture.texture })
+  new THREE.MeshBasicMaterial({ map: composer.readBuffer.texture })
 );
+
 // const plane = new THREE.Mesh( planelikeGeometry, new THREE.MeshBasicMaterial( { color: 'red' } ) );
 
 // plane.position.set(0,100,-500);
@@ -50,22 +128,25 @@ const backGround = new THREE.Mesh(
 backGround.position.set(0.5, -0.5, -0.01);
 // sceneRTT.add(backGround);
 
+// const colors = [
+//   "#568ca1",
+//   "#4fc1ff",
+//   "#4ec9b0",
+//   "#d4d4d4",
+//   "#9cdcfe",
+//   "#ce9178",
+//   "#dcdcaa",
+//   "#b5cea8",
+//   "#6a9955",
+//   "#569cd6",
+//   "#c586c0",
+// ];
 const colors = [
-  "#568ca1",
-  "#4fc1ff",
-  "#4ec9b0",
-  "#d4d4d4",
-  "#9cdcfe",
-  "#ce9178",
-  "#dcdcaa",
-  "#b5cea8",
-  "#6a9955",
-  "#569cd6",
-  "#c586c0",
+  "#42db82",
 ];
 const words = [];
 const wordsToAnm = [];
-function makeWork(props) {
+function makeWord(props) {
   const height = 0.05;
   const minWidth = 0.05;
   const maxWidth = 0.3;
@@ -112,10 +193,10 @@ function makeWork(props) {
 
 let n = [0, 0];
 for (let i = 0; i < 30; i++) {
-  n = makeWork({ x: n[0], y: n[1], anm: true });
+  n = makeWord({ x: n[0], y: n[1], anm: true });
 }
 
-const [_x, _y, bat] = makeWork({ x: 0.4, y: 0.9, width: 0.2, color: "red" });
+const [_x, _y, bat] = makeWord({ x: 0.4, y: 0.9, width: 0.2, color: "red" });
 bat.scale.x = 0.2;
 console.log(bat);
 
@@ -129,58 +210,13 @@ console.log(bat);
 // const mesh = new THREE.Mesh(geometry, material);
 // sceneRTT.add(mesh);
 
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.set(0.25, -0.25, 1);
-scene.add(camera);
 
-const cameraRTT = new THREE.OrthographicCamera(-0.1, 1.1, 0.1, -1.1, 1, 3);
-sceneRTT.add(cameraRTT);
-cameraRTT.position.set(0, 0, 1);
 
-// Controls
-// const controls = new OrbitControls(cameraRTT, canvas);
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
@@ -220,9 +256,12 @@ const tick = () => {
 
   // Render first scene into texture
 
-  renderer.setRenderTarget(rtTexture);
-  renderer.clear();
-  renderer.render(sceneRTT, cameraRTT);
+  // renderer.setRenderTarget(rtTexture);
+  // renderer.clear();
+  // renderer.render(sceneRTT, cameraRTT);
+  composer.render();
+  // plane.material =  new THREE.MeshBasicMaterial({ map: composer.readBuffer.texture })
+  
 
   // Render full screen quad with generated texture
 
@@ -233,8 +272,11 @@ const tick = () => {
   // (using first scene as regular texture)
 
   renderer.setRenderTarget(null);
-  // renderer.render(scene, camera);
-  renderer.render(sceneRTT, cameraRTT);
+  renderer.render(scene, camera);
+  // renderer.render(sceneRTT, cameraRTT);
+  // composer.render();
+  // console.log(composer.writeBuffer.texture)
+  
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
