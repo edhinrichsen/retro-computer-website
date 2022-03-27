5e1fe76aaedbdb8072cf7d0aab2a6ca29222dba3
Edward Hinrichsen <edward.w.hinrichsen@gmail.com>
2022-03-27 22:49:21 +1100

    add make

--- src/terminal/bash.ts
+++ src/terminal/bash.ts
@@ -36,8 +36,46 @@ export default function Bash(print: (s: string, md?: boolean) => void) {
         print(`\n${operation.at(-1)?.name}:not a directory`);
         return;
       }
-      
+
       path = operation as any;
+    },
+    echo: (args: string[]) => {
+      const str = args.join(" ");
+      print(`\n${str}`);
+    },
+    pwd: (args: string[]) => {
+      let out = "";
+      for (let i = 0; i < path.length; i++) {
+        out += path[i].name;
+        if (i !== 0 && i < path.length - 1) out += "/";
+      }
+      print(`\n${out}`);
+    },
+    mkdir: (args: string[]) => {
+      if (args.length === 0) {
+        print(`\nMissing directory name`);
+        return;
+      }
+      const out = fileSystem.make(path, args[0], "folder");
+      if (out === "bad_args") {
+        print(`\nMissing directory name`);
+      } else if (out === "bad_path") {
+        print(`\nNo such file or directory`);
+      } else if (out === "file_exists") {
+        print(`\nFile or directory already exists`);
+      }
+    },
+    touch: (args: string[]) => {
+      if (args.length === 0) {
+        print(`\nMissing file name`);
+        return;
+      }
+      const out = fileSystem.make(path, args[0], "file");
+      if (out === "bad_args") {
+        print(`\nMissing file name`);
+      } else if (out === "bad_path") {
+        print(`\nNo such file or directory`);
+      }
     },
     show: (args: string[]) => {
       if (args.length === 0) {
@@ -47,19 +85,33 @@ export default function Bash(print: (s: string, md?: boolean) => void) {
       const file = fileSystem.goto(path, args[0])?.at(-1);
       if (!file) {
         print(`\nNo such file or directory`);
-        return
+        return;
       }
 
       if (!("data" in file)) {
         print(`\n${file.name}:not a file`);
-        return
+        return;
       }
 
       print(file.data, true);
     },
-    echo: (args: string[]) => {
-      const str = args.join(" ");
-      print(`\n${str}`);
+    nano: (args: string[]) => {
+      if (args.length === 0) {
+        print(`\nMissing filename`);
+        return;
+      }
+      const file = fileSystem.goto(path, args[0])?.at(-1);
+      if (!file) {
+        print(`\nNo such file or directory`);
+        return;
+      }
+
+      if (!("data" in file)) {
+        print(`\n${file.name}:not a file`);
+        return;
+      }
+
+      print(file.data);
     },
   };
 
