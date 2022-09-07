53453622672acff9319e4b98407e3d6756e24b51
Edward Hinrichsen <edward.w.hinrichsen@gmail.com>
2022-09-07 22:42:08 +1000

    remove source-map

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "../dist"),
   },
-  devtool: "source-map",
+  devtool: false,
   plugins: [
     new CopyWebpackPlugin({
       patterns: [{ from: path.resolve(__dirname, "../static") }],
